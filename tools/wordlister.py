330177ec9b64718a94c956910b8b61a446b1aea6
Farkas Bence <farkasb0420@gmail.com>
2026-05-10 11:51:11 +0200

    yml and py script

---
+++ tools/wordlister.py
@@ -0,0 +1,19 @@
+from os import listdir
+from os.path import isfile, join
+
+def update_words(PATH, FILE):
+    files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
+    xmls = []
+
+    for file in files:
+        if file.endswith("-TEI.xml"):
+            xmls.append(file.split("-TEI.xml")[0].lower())
+
+    xmls.sort()
+    
+    with open(PATH + FILE, "w") as f:
+        for word in xmls:
+            f.write(word + "\n")
+
+update_words("lemma/hungarian", "/words_hungarian.txt")
+update_words("lemma/javanese", "/words_javanese.txt")
